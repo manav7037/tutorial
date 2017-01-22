8bc26b734e6b36261ea6f5497c44036f9cead8f2
manav7037 <manav7037@gmail.com>
2017-01-22 23:06:40 +0530

    game stone,paper,seissor

---
+++ stone paper scissor.py
@@ -0,0 +1,52 @@
+ # programe name - game stone ,paper , scissor
+ # programmer name - manav patel
+ # date & time - 22/01/2017 23:03 
+while(1==1):
+ print "welcome to game of stone paper scissor"
+
+ print"player1 please enter your input"
+ a= int(raw_input("1. stone 2.paper 3.scissor"))
+ print"player2 please enter your input"
+ b= int(raw_input("1. stone 2.paper 3.scissor"))
+
+ # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+ if a==1:
+    if b==1:
+     print "match draw"
+
+ if a==1:
+    if b==2:
+     print "player two win"
+
+ if a==1:
+    if b==3:
+     print "player one win"
+
+ # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     
+ if a==2:
+    if b==1:
+     print "player one win"
+     
+ if a==2:
+    if b==2:
+     print "match draw"
+
+ if a==2:
+    if b==3:
+     print "player two win"
+ # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     
+ if a==3:
+    if b==1:
+     print "player two win"
+     
+ if a==3:
+    if b==2:
+     print "player one win"
+
+ if a==3:
+    if b==3:
+     print "match draw"
+ # -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------     

6724b28e44f13a87017c8e763200c253067cd692
manav7037 <manav7037@gmail.com>
2017-01-26 18:33:48 +0530

    factorial project

---
+++ factorial.py
@@ -0,0 +1,7 @@
+while(1==1):
+    a=int(raw_input("enter the number of which you want factorial"))
+    b=1
+    while a>0:
+      b=b*a
+      a=a-1
+    print "factorial no of input value is " ,b 
